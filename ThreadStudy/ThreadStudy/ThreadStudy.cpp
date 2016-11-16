ad9f6104bcecc74a758cf6e2e29ed99a529de44b
shang-qd <892171682@qq.com>
2016-11-16 18:18:10 +0800

    Signed-off-by: shang-qd <892171682@qq.com>

--- ThreadStudy/ThreadStudy/ThreadStudy.cpp
+++ ThreadStudy/ThreadStudy/ThreadStudy.cpp
@@ -2,16 +2,41 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
-#include <vector>
-#include <thread>
-#include <string>
-#include <mutex>
+#include <iostream>  
+#include <thread>  
+#include <mutex>  
+#include <condition_variable>  
 
 using namespace std;
-static mutex g_mutex;
+
+static mutex m;
+static condition_variable cond;
+static int LOOP = 10;
+static int flag = 0;
+
+void fun(int id)
+{
+	for (int i = 0; i < LOOP; i++)
+	{
+		unique_lock<mutex> lk(m);
+		//一定要用循环判断，若是if多个阻塞线程唤醒后同时处于临界区
+		while (id != flag)  
+			cond.wait(lk);
+		char c = 'A' + id;
+		cout << c << endl;
+		flag = (flag + 1) % 3;
+		cond.notify_all();
+	}
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	thread B(fun, 1);
+	thread C(fun, 2);
+	fun(0);
+	cout << endl;
+	B.join();
+	C.join();
 	return 0;
 }
